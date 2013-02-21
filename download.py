ea6d2f1f5a356609a8d92a0b9b37f34f67e50cbc
David Wilemski <david@davidwilemski.com>
2013-02-21 02:50:38 -0500

    merge download.py and merge.py and store pages in memory

--- download.py
+++ download.py
@@ -1,17 +1,22 @@
 import json
 import urllib2
 
-n = 0
+all_posts = []
 access_token = 'PUT_YOUR_ACCESS_TOKEN_HERE'
-url = "https://graph.facebook.com/160284404008688/feed?access_token=" + access_token
+url = "https://graph.facebook.com/160284404008688/feed?limit=250&access_token=" + access_token
+
 while(True):
 	response = urllib2.urlopen(url)
 	json_str = response.read()
 
-	f = open('data/' + str(n) + '.json', 'w')
-	f.write(json_str)
-	f.close()
-
 	data = json.loads(json_str)
+	posts = data['data']
+	all_posts += posts
+
+	if 'paging' not in data:
+        break
 	url = data['paging']['next']
-	n += 1
+
+out_file = open('hackersandhustlers.json', 'w')
+out_file.write(json.dumps(all_posts))
+out_file.close()
