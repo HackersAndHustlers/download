bb587847e9886518cec85e328e1ed0e07c59cf6b
David Wilemski <david@davidwilemski.com>
2013-02-21 12:23:13 -0500

    fix indentation errors

--- download.py
+++ download.py
@@ -6,16 +6,16 @@ access_token = 'PUT_YOUR_ACCESS_TOKEN_HERE'
 url = "https://graph.facebook.com/160284404008688/feed?limit=250&access_token=" + access_token
 
 while(True):
-	response = urllib2.urlopen(url)
-	json_str = response.read()
+    response = urllib2.urlopen(url)
+    json_str = response.read()
 
-	data = json.loads(json_str)
-	posts = data['data']
-	all_posts += posts
+    data = json.loads(json_str)
+    posts = data['data']
+    all_posts += posts
 
-	if 'paging' not in data:
+    if 'paging' not in data:
         break
-	url = data['paging']['next']
+    url = data['paging']['next']
 
 out_file = open('hackersandhustlers.json', 'w')
 out_file.write(json.dumps(all_posts))
